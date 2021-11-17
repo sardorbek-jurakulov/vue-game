9f70b78f11d52b0bc03e4dafcd0911208b1d9172
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-11-17 23:58:25 +0500

    health limit is fixed

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,48 @@ new Vue({
   data: {
     gamerHealth: 100,
     monsterHealth: 100,
+    gameIsOver: false
   },
   methods: {
     attack: function() {
+      if(this.gameIsOver) {
+        return;
+      }
       this.gamerHealth = this.gamerHealth - Math.floor(Math.random() * 10);
       this.monsterHealth = this.monsterHealth - Math.floor(Math.random() * 10);
+      this.gameOverChecker();
+    },
+    specialAttack: function() {
+      if(this.gameIsOver) {
+        return;
+      }
+      this.gamerHealth = this.gamerHealth - Math.floor(Math.random() * 10);
+      this.monsterHealth = this.monsterHealth - Math.floor(Math.random() * 15);
+      this.gameOverChecker();
+    },
+    heal: function() {
+      if(this.gameIsOver) {
+        return;
+      }
+      this.gamerHealth = this.gamerHealth + Math.floor(Math.random() * 10);
+    },
+    giveUp: function() {
+      if(this.gameIsOver) {
+        return;
+      }
+      this.gamerHealth = 0;
+      this.gameIsOver = true;
+    },
+    gameOverChecker: function() {
+      if((this.gamerHealth === 0 || this.gamerHealth < 0) || (this.monsterHealth === 0 || this.monsterHealth < 0)) {
+        this.gameIsOver = true;
+        if(this.gamerHealth === 0 || this.gamerHealth < 0) {
+          this.gamerHealth = 0
+        }
+        if(this.monsterHealth === 0 || this.monsterHealth < 0) {
+          this.monsterHealth = 0
+        }
+      }
     }
   }
 });
